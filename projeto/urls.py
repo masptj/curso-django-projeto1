df6745d81aaab1428511c35429829b0a98866203
Masptj <masptj@gmail.com>
2023-08-06 21:42:22 -0300

    até aqui temos as páginas sobre e contato

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,13 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-def mysobre(request):
-    return HttpResponse("Página sobre")
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/', mysobre)
+    path('', include('recipes.urls'))
 ]
